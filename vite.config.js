632831ee5b436f3688ab762211fddd615fd5c5b7
= <=>
2025-02-21 17:32:58 +0800

    build again

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/letter/',
-  build: {
-    outDir: 'docs'
-  }
+  base: '/letter/'
 })
